e8e8d6b395c132ac7a0e68c55126c01150e58a76
kimminji2302 <kimminji2302@gmail.com>
2026-03-17 16:39:11 +0900

    first commit

---
+++ P97_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        // std::cout << i << "\n";
+        for(int j = 4; j >= i; j--)
+        {
+            std::cout << " ";
+        }
+        for (int l = 0; l <= i -1; l++)
+        {
+            std::cout << i - l;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
---
+++ p97_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    for(int i = 1; i <= 5; ++i)
+    {
+        // std::cout << i << "\n";
+        for(int j = 1; j <= i; ++j)
+        {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }
+}
